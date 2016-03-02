5a6e49b8b28e5dee424c83767128f99ce021b233
wanbei <liuluopingyue@sina.com>
2016-03-02 11:06:53 +0800

    Create convert.js

---
+++ convert.js
@@ -0,0 +1,34 @@
+window.onload = function(){
+		
+		function timeConvert() {
+			this.$inputtime = document.querySelector("#inputtime");
+			this.$submit = document.querySelector("#submit");
+			this.$outputtime = document.querySelector("#outputtime");
+			this.init();
+		}
+		timeConvert.prototype.init = function() {
+			// body...
+			this.$inputtime.value = this.getCurrentTimeStamp();
+			this.bindCovert();
+		}
+		timeConvert.prototype.bindCovert = function() {
+			var _this = this;
+			_this.$submit.addEventListener("click", function(evt){
+				evt.stopPropagation();
+				evt.preventDefault();
+				if(!_this.$inputtime.value) {
+					alert("你到是输入时间戳呀！！！");
+					return;
+				}
+				var date = new Date(Number(_this.$inputtime.value) * 1000)
+				
+				_this.$outputtime.value = date.getFullYear() + "/" + (date.getMonth() + 1) + "/" + date.getDate() + "/ " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+			}, false);
+		}
+		timeConvert.prototype.getCurrentTimeStamp = function() {
+			var timeStamp = Math.floor(new Date().getTime() / 1000);
+			return timeStamp;
+		}
+		new timeConvert();
+	}
