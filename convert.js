baeb0eeedc8a64741bc420ca84150852b5b4ef2e
wanbei <liuluopingyue@sina.com>
2016-03-03 10:52:30 +0800

    Added files via upload

--- convert.js
+++ convert.js
@@ -5,11 +5,17 @@ window.onload = function(){
 			this.$submit = document.querySelector("#submit");
 			this.$outputtime = document.querySelector("#outputtime");
 			this.$err = document.querySelector(".err");
+
+			this.$inputtime1 = document.querySelector("#inputtime1");
+			this.$submit1 = document.querySelector("#submit1");
+			this.$outputtime1 = document.querySelector("#outputtime1");
+			this.$err1 = document.querySelector("#err1");
 			this.init();
 		}
 		timeConvert.prototype.init = function() {
 			// body...
-			this.$inputtime.value = this.getCurrentTimeStamp();
+			this.getCurrentTimeStamp();
+			this.getCurrentTime();
 			this.bindCovert();
 		}
 		timeConvert.prototype.bindCovert = function() {
@@ -19,6 +25,13 @@ window.onload = function(){
 				evt.preventDefault();
 				_this.renderDate();
 			}, false);
+
+			this.$submit1.addEventListener("click", function(evt){
+				evt.stopPropagation();
+				evt.preventDefault();
+				_this.renderTimeStamp();
+			}, false);
+
 		}
 		timeConvert.prototype.renderDate = function() {
 			var date;
@@ -40,11 +53,30 @@ window.onload = function(){
 				date = new Date(Number(this.$inputtime.value))
 			}
 			
-			this.$outputtime.value = date.getFullYear() + "/" + (date.getMonth() + 1) + "/" + date.getDate() + "/ " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+			this.$outputtime.value = date.getFullYear() + "/" + (date.getMonth() + 1) + "/" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+		}
+		timeConvert.prototype.renderTimeStamp = function() {
+			var date, matchRes = this.$inputtime1.value.match(/^([0-9]{4})\/([0-9]{1,2})\/([0-9]{1,2})\s+([0-9]{2}):([0-9]{2}):([0-9]{2})$/);
+			
+			if(!matchRes || matchRes.length != 7 || Number(matchRes[1]) < 1000 || Number(matchRes[2]) > 12 || Number(matchRes[2]) <= 0 || Number(matchRes[3]) > 31 || Number(matchRes[3]) == 0 || Number(matchRes[4]) > 24 || Number(matchRes[5]) > 59 || Number(matchRes[6]) > 59) {
+				this.$err1.style.display = "inline-block";
+				this.$outputtime1.value = '';
+				this.$inputtime1.focus();
+				return;
+			}
+			if(this.$err1.style.display == "inline-block") {
+				this.$err1.style.display = "none";	
+			}
+			date = new Date(this.$inputtime1.value);
+			this.$outputtime1.value = date.getTime() / 1000;
+		}
+		timeConvert.prototype.getCurrentTime = function() {
+			var date = new Date();
+			this.$inputtime1.value = date.getFullYear() + "/" + (date.getMonth() + 1) + "/" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
 		}
 		timeConvert.prototype.getCurrentTimeStamp = function() {
 			var timeStamp = Math.floor(new Date().getTime() / 1000);
-			return timeStamp;
+			this.$inputtime.value = timeStamp;
 		}
 		new timeConvert();
-	}
+}
